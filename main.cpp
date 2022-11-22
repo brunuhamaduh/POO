afa640a0c52ca31c6ec77ec394a281be1d74914f
brunuhamaduh <brunuhamaduh@gmail.com>
2022-11-22 18:53:21 +0000

    new UI

--- main.cpp
+++ main.cpp
@@ -15,36 +15,24 @@ class Coelho: private Animal
 
 };
 
-void print_size(Terminal& t)
+void mostra(Terminal &t, Window &comando, Window &info, Window &reserva)
 {
-    std::ostringstream o;
-    o << std::setw(7) << t.getNumCols() << "x" << t.getNumRows();
-    std::string str = o.str();
-    t.clear();
-    t << set_color(0) << move_to(t.getNumCols()-str.length(), t.getNumRows()-1) << str;
+    string input;
+    comando << "Comando: \n";
+    info << "Instante: \n";
+    info << "Animais Vivos: \n";
+    info << "Comida: \n";
+    info << "Extremos: \n";
+    comando >> input;
 }
 
 int main()
 {
-    int instante = 0, animaisvivos = 0, quantidadecomida = 0;
-    int tamanho = 20;
     Terminal &t = Terminal::instance();
 
-    for(int i=0; i<=tamanho*2; i++)
-    {
-        for(int j=0; j<=tamanho; j++)
-        {
-            t << move_to(i, j)  << "_";
-        }
-    }
-
-    t << move_to(tamanho*2+2,0) << "Instante: " << instante;
-    t << move_to(tamanho*2+2,1) << "Animais Vivos: " << animaisvivos;
-    t << move_to(tamanho*2+2,2) << "Quantidade de comida: " << quantidadecomida;
-    t << move_to(tamanho*2+2,3) << "Extremos: (0,0) (" << tamanho << ",0) (" << tamanho << "," << tamanho << ") (0," << tamanho << ")";
-
-    t << move_to(0,22) << "Comando = ";
-
-    t.getchar();
-    print_size(t);
+    //30x120
+    Window comando = Window(0, 27, 100, 3);
+    Window info = Window(100, 0, 20, 30);
+    Window reserva = Window(0, 0, 100, 27);
+    mostra(t, comando, info, reserva);
 }
